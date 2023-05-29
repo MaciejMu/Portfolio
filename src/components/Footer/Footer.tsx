93ee0108320a47ce0254d17003c43e9804d5cfee
Maciej Muter <116006124+MaciejMu@users.noreply.github.com>
2023-05-29 22:30:11 +0200

    fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,12 +3,7 @@ import style from "./Footer.module.scss";
 
 const Footer = () => {
   return (
-    <footer
-      className={style.footer}
-      data-aos="fade-up"
-      data-aos-delay="400"
-      data-aos-offset="-100"
-    >
+    <footer className={style.footer}>
       <ul>
         <Link href={"#AboutMe"}>
           <li> O mnie</li>
